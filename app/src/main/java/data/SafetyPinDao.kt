b4fbfa7f0e082089308313a3323dea80ec3ff116
Sakshi <chauhansakshi4m@gmail.com>
2025-10-18 19:09:03 +0530

    Added SafetyPinDao

---
+++ app/src/main/java/data/SafetyPinDao.kt
@@ -0,0 +1,19 @@
+package data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface SafetyPinDao {
+
+    @Insert
+    suspend fun insert(safetyPin: SafetyPin)
+
+    @Query("SELECT * FROM SafetyPin")
+    suspend fun getAllPins(): List<SafetyPin>
+
+    @Delete
+    suspend fun delete(safetyPin: SafetyPin)
+}
