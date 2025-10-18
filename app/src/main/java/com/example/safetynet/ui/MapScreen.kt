517256d81a7ac3738c892068b002813cd2227c56
Sakshi <chauhansakshi4m@gmail.com>
2025-10-18 13:16:57 +0530

    added MapViewModel to get user's location

--- app/src/main/java/com/example/safetynet/ui/MapScreen.kt
+++ app/src/main/java/com/example/safetynet/ui/MapScreen.kt
@@ -1,80 +1,19 @@
-package com.example.safetynet.ui
-
-import android.content.pm.PackageManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
+import com.example.safetynet.ui.MapViewModel
 import com.google.maps.android.compose.rememberCameraPositionState
 import timber.log.Timber
+
 
 @Composable
 fun MapScreen(mapViewModel: MapViewModel) {
 
-    // Initialize the camera position state, which controls the camera's position on the map
-    val cameraPositionState = rememberCameraPositionState()
+    
 
-    // obtain the current context
-    val context = LocalContext.current
-
-    // observe the user's location from the viewmodel
-    val userLocation by mapViewModel.userLocation
-    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
-
-    // handle permission requests for accessing fine location
-    val permissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            // fetch the user's location and update the camera if permission is granted
-            mapViewModel.fetchUserLocation(context, fusedLocationClient)
-        } else {
-            // handle the case when permission is denied
-            Timber.e("Location was denied by the yser.")
-        }
-    }
-
-    // request the location permission when the composable is launched
-    LaunchedEffect(Unit) {
-        when (PackageManager.PERMISSION_GRANTED) {
-            // check if the location permission is already granted
-            ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                // fetch the user's location and update the camera
-                mapViewModel.fetchUserLocation(context, fusedLocationClient)
-            } else -> {
-              // request the location permission if it has not been granted
-                permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
-            }
-        }
-    }
-
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-        // if the user's location is available, place a marker on the map
-        userLocation?.let {
-            Marker  (
-                state = MarkerState(position = it), // place the marker at the user's location
-                title = "Your Location", // set the title for the marker
-                snippet = "This is where you are currently located." // set the snipped for the marker
-            )
-
-            // move the camera to the user's location with a zoom level of 10f
-            cameraPositionState.position = CameraPosition.fromLatLngZoom(it, 10f)
-        }
-    }
 
 }
--- app/src/main/java/com/example/safetynet/ui/MapViewModel.kt
+++ app/src/main/java/com/example/safetynet/ui/MapViewModel.kt
@@ -8,36 +8,32 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.model.LatLng
+import data.LocationRepository
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
-class MapViewModel: ViewModel() {
+class MapViewModel(private val locationRepository: LocationRepository): ViewModel() {
+
 
     // state to hold the user's location as LatLng
     private val _userLocation = mutableStateOf<LatLng?>(null)
+    // public exposed state
     val userLocation: State<LatLng?> = _userLocation
 
-    // function to fetch the user's location and update the state
-    fun fetchUserLocation(context: Context, fusedLocationClient: FusedLocationProviderClient) {
 
-        if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            try {
-                // fetch the last known location
-                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-                    location?.let {
-                        // update the user's location in the state
-                        val userLatLng = LatLng(it.latitude, it.longitude)
-                        _userLocation.value = userLatLng
-                    }
+    public fun fetchUserLocation() {
+        viewModelScope.launch {
+            locationRepository.getCurrentLocation()
+                .onSuccess { latLng ->
+                    _userLocation.value = latLng
                 }
-            } catch (e: SecurityException) {
-                Timber.e("Permission for location access was revoke: ${e.localizedMessage}")
-            }
-        } else {
-            Timber.e("Location permission is not granted")
+                .onFailure { exception ->
+                    Timber.e(exception, "Failed to fetch user location")
+                }
         }
-
     }
 
 }
