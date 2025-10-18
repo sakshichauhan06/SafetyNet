0903097307cfadcfc7b3b3957cc90c2eb1ef4641
Sakshi <chauhansakshi4m@gmail.com>
2025-10-18 16:04:28 +0530

    To get the user's current location

--- app/src/main/java/com/example/safetynet/MainActivity.kt
+++ app/src/main/java/com/example/safetynet/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.safetynet
 
+import MapScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.safetynet.ui.theme.SafetyNetTheme
-import com.example.safetynet.ui.MapScreen
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import com.example.safetynet.ui.MapViewModel
+import com.example.safetynet.ui.theme.SafetyNetTheme
+import com.google.android.gms.location.LocationServices
+import data.LocationRepository
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +18,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SafetyNetTheme {
-                val mapViewModel = MapViewModel()
+                val context = LocalContext.current
+                val fusedLocationClient = remember {
+                    LocationServices.getFusedLocationProviderClient(context)
+                }
+                val locationRepository = remember { LocationRepository(fusedLocationClient) }
+                val mapViewModel = remember { MapViewModel(locationRepository) }
+
                 MapScreen(mapViewModel)
             }
         }
--- app/src/main/java/com/example/safetynet/ui/MapScreen.kt
+++ app/src/main/java/com/example/safetynet/ui/MapScreen.kt
@@ -1,11 +1,19 @@
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import com.example.safetynet.ui.MapViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import timber.log.Timber
 
@@ -13,7 +21,59 @@ import timber.log.Timber
 @Composable
 fun MapScreen(mapViewModel: MapViewModel) {
 
-    
+    val userLocation by mapViewModel.userLocation
+    val context = LocalContext.current
+    val cameraPositionState = rememberCameraPositionState()
 
+    // Permission launcher
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if(isGranted) {
+            mapViewModel.fetchUserLocation()
+        } else {
+            Timber.e("Location permission denied")
+        }
+    }
+
+    // Request permission when screen loads
+    LaunchedEffect(Unit) {
+        when {
+            ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED -> {
+                mapViewModel.fetchUserLocation()
+            } else -> {
+            permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    GoogleMap(
+        modifier = Modifier
+            .fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        userLocation?.let { location ->
+            Marker(
+                state = MarkerState(position = location),
+                title = "Your Location"
+            )
+            LaunchedEffect(location) {
+                cameraPositionState.animate(
+                    CameraUpdateFactory.newLatLngZoom(
+                        location, 15f
+                    )
+                )
+            }
+        }
+    }
 
 }
+
+//@Preview
+//@Composable
+//fun MapScreenPreview() {
+//
+//}
