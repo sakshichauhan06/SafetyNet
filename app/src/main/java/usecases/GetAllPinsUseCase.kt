f4b753ce3b359ca5cbe07bdce236d13d047bc45e
Sakshi <chauhansakshi4m@gmail.com>
2025-10-21 17:37:14 +0530

    Added Severity sorting

--- app/src/main/java/usecases/GetAllPinsUseCase.kt
+++ app/src/main/java/usecases/GetAllPinsUseCase.kt
@@ -3,6 +3,7 @@ package usecases
 import com.google.android.gms.maps.model.LatLng
 import data.SafetyPin
 import data.SafetyPinRepository
+import domain.SeverityLevel
 import utils.LocationUtils
 
 class GetAllPinsUseCase(private val repository: SafetyPinRepository) {
@@ -33,7 +34,23 @@ class GetAllPinsUseCase(private val repository: SafetyPinRepository) {
             distance <= MAX_RADIUS_METERS
         }
 
-        // 4. wrap in Result / return filtered list
-        return Result.success(radiusFilteredList)
+        // 4. sort by severity (red -> orange -> yellow -> green)
+        // and then by distance
+        val sortedPins = radiusFilteredList.sortedWith(compareBy(
+            { severityPriority(it.severity) },
+            { LocationUtils.calculateDistance(LatLng(it.latitude, it.longitude), userLocation) }
+        ))
+
+        // 5. wrap in Result / return filtered list
+        return Result.success(sortedPins)
+    }
+
+    private fun severityPriority(severity: SeverityLevel): Int {
+        return when (severity) {
+            SeverityLevel.RED -> 0
+            SeverityLevel.ORANGE -> 1
+            SeverityLevel.YELLOW -> 2
+            SeverityLevel.GREEN -> 3
+        }
     }
 }
