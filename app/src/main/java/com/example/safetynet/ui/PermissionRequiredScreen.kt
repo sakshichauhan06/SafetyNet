b2a2a38a93d30d054fec4e6453bc532c73a48101
Sakshi <chauhansakshi4m@gmail.com>
2025-12-07 13:08:37 +0530

    Added color attributes

---
+++ app/src/main/java/com/example/safetynet/ui/PermissionRequiredScreen.kt
@@ -0,0 +1,4 @@
+package com.example.safetynet.ui
+
+class PermissionRequiredScreen {
+}
--- app/src/main/java/com/example/safetynet/ui/theme/Color.kt
+++ app/src/main/java/com/example/safetynet/ui/theme/Color.kt
@@ -2,6 +2,34 @@ package com.example.safetynet.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Primary Colors
+val ColorPrimary = Color(0xFF0055FF) // Blue Ribbon
+val ColorPrimaryVariant = Color(0xFF003D99)
+val ColorOnPrimary = Color.White
+
+// Secondary Colors
+val ColorSecondary = Color(0xFF00BCD4)
+val ColorSecondaryVariant = Color(0xFF007B90)
+val ColorOnSecondary = Color.White
+
+// BackGround Colors
+val ColorLightBackground = Color(0xFFE0E0FF)
+val ColorOnLightBackground = Color(0xFF0A0A1C)
+
+val ColorDarkBackground = Color(0xFF0A0A1C)
+val ColorOnDarkBackground = Color.White
+
+// Surface Colors
+val ColorSurface = Color(0xFFE0E0E0)
+val ColorOnSurface = Color(0xFF0A0A1C)
+
+// Error Colors
+val ColorError = Color(0xFFD32F2F)
+val ColorOnError = Color.White
+
+
+
+// Neutral Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +37,4 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
